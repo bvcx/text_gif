2328a19ba680e0e9f3c7536f8924bdad3086400e
John Brattin <john.brattin@gmail.com>
2016-07-03 18:04:48 +0000

    better validation, better error messages

--- app.rb
+++ app.rb
@@ -3,20 +3,30 @@ require 'net/http'
 require 'json'
 
 get '/' do
-  @search_text = params['s']
-  @phone_number = params['p']
+  @search_text = valid_search(params['s'])
+  @phone_number = valid_phone(params['p'])
   erb :index
 end
 
 post '/' do
-  @search_text = params['s']
-  @phone_number = params['p']
+  @search_text = valid_search(params['s'])
+  @phone_number = valid_phone(params['p'])
   erb :index
 end
 
 helpers do
-  def valid(text)
-    !(text.nil? || text.empty?)
+  def valid_search(search)
+    return nil if search.nil?
+    return nil if search.empty?
+    return search
+  end
+
+  def valid_phone(phone)
+    return nil if phone.nil?
+    phone.gsub!(/[^1-9]/, "")
+    return "+1" + phone if phone.length == 10
+    return "+" + phone if phone.length == 11 && phone.start_with?(1)
+    return nil
   end
 
   def img(link)
