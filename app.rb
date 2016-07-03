3c9d6b0e0cb138c63edccdd961025309519dd9f9
John Brattin <john.brattin@gmail.com>
2016-07-03 15:47:20 +0000

    make calls to giphy

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'net/http'
+require 'json'
 
 get '/' do
   @search_text = params['s']
@@ -15,5 +17,19 @@ end
 helpers do
   def valid(text)
     !(text.nil? || text.empty?)
+  end
+
+  def img(link)
+    "<img src=#{link} />"
+  end
+
+  def call_giphy(search_text)
+    search_text = CGI::escape(search_text)
+    url = "http://api.giphy.com/v1/gifs/search?q=#{search_text}&api_key=dc6zaTOxFJmzC&limit=1"
+    puts url
+    response = Net::HTTP.get_response(URI.parse(url))
+    buffer = response.body
+    result = JSON.parse(buffer)
+    result['data'][0]['images']['fixed_width']['url']
   end
 end
