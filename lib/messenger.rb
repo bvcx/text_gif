628f76a7c2071f5133e186c0b672e3b46d1a99e8
John Brattin <john.brattin@gmail.com>
2016-07-03 19:22:13 +0000

    handle twilio exceptions

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -3,17 +3,23 @@ require 'twilio-ruby'
 
 module Messenger
   def self.send_url(image_url, phone_number)
-    return if environment_not_set_up
+    return false if environment_not_set_up
     client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
     message_body = ""
     twilio_number = ENV['TWILIO_FROM_NUMBER']
-    message = client.messages.create(
-      from: twilio_number,
-      to: phone_number,
-      body: message_body,
-      media_url: image_url
-    )
-    puts "sent gif #{image_url} to #{phone_number}"
+    begin
+      client.messages.create(
+        from: twilio_number,
+        to: phone_number,
+        body: message_body,
+        media_url: image_url
+      )
+      puts "sent gif #{image_url} to #{phone_number}"
+      return true
+    rescue Twilio::REST::RequestError => e
+      puts e
+      return false
+    end
   end
 
   def self.environment_not_set_up
