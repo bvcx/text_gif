e127596809345c6edb02447bd6f439e1fedfa959
John Brattin <john.brattin@gmail.com>
2016-07-03 17:07:49 +0000

    handle empty response from giphy

--- app.rb
+++ app.rb
@@ -20,16 +20,21 @@ helpers do
   end
 
   def img(link)
-    "<img src=#{link} />"
+    "<img src=#{link} />" unless link.nil?
   end
 
   def call_giphy(search_text)
     search_text = CGI::escape(search_text)
     url = "http://api.giphy.com/v1/gifs/search?q=#{search_text}&api_key=dc6zaTOxFJmzC&limit=1"
-    puts url
     response = Net::HTTP.get_response(URI.parse(url))
     buffer = response.body
     result = JSON.parse(buffer)
-    result['data'][0]['images']['fixed_width']['url']
+    result
+  end
+
+  def handle_response(response)
+    return nil if response['data'].empty?
+    @full_gif = response['data'][0]['images']['original']['url']
+    @small_gif = response['data'][0]['images']['fixed_width']['url']
   end
 end
